8a8a108739d1ea1a8d474d05b0ccb3aa1fa3f2d9
Yunha Choi <61413047+choiyhking@users.noreply.github.com>
2024-01-19 20:30:47 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ import cv2
 import time
 import face_detection
 
-
 def draw_faces(im, bboxes):
     for bbox in bboxes:
         x0, y0, x1, y1 = [int(_) for _ in bbox]
@@ -12,21 +11,28 @@ def draw_faces(im, bboxes):
 
 
 if __name__ == "__main__":
-    impaths = "images"
+    impaths = "/DSFD-Pytorch-Inference/images/fddb_images/2003/01/01/big"
     impaths = glob.glob(os.path.join(impaths, "*.jpg"))
     detector = face_detection.build_detector(
         "DSFDDetector",
         max_resolution=1080
     )
+
+	  total_processing_time = 0.0
+
     for impath in impaths:
         if impath.endswith("out.jpg"): continue
         im = cv2.imread(impath)
+        
         print("Processing:", impath)
         t = time.time()
         dets = detector.detect(
             im[:, :, ::-1]
         )[:, :4]
-        print(f"Detection time: {time.time()- t:.3f}")
+        processing_time = time.time() - t
+        total_processing_time += processing_time
+        print(f"Detection time: {processing_time:.3f}")
+        
         draw_faces(im, dets)
         imname = os.path.basename(impath).split(".")[0]
         output_path = os.path.join(
@@ -35,4 +41,5 @@ if __name__ == "__main__":
         )
 
         cv2.imwrite(output_path, im)
-        
+
+    print("Total processing time: ", total_processing_time)
